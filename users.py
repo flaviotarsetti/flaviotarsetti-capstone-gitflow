d23038535e97ffe68042e686f886e5838a5b31c0
Flavio Tarsetti <flavio.tarsetti@uc.cl>
2022-03-27 01:30:21 -0300

    Fix variable name

--- users.py
+++ users.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 def get_top_users(tweets):
     users = Counter(d['user']["username"] for d in tweets[0:100])
-    top_users = sorted(date_counts.items(), key=lambda item: item[1], reverse=True)
+    top_users = sorted(users.items(), key=lambda item: item[1], reverse=True)
     print('{:^4}{:^30}{:^20}'.format("N°","Username", "Number of tweets"))
     for i in range(10):
         print('{:^4}{:^30}{:^20}'.format(i+1, top_users[i][0], top_users[i][1]))
